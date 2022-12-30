a13e9a76dc71bd3d295d5d8a8a489c4e51ee6718
Virgil Surin <virgil.surin@student.umons.ac.be>
2022-12-30 16:52:56 +0100

    CLIQUE and BK algo

--- python/BK.py
+++ python/BK.py
@@ -1,4 +1,4 @@
-
+# <<<=== START OF UTILITY ===>>>
 class Graph:
     def __init__(self, order):
         self.order = order
@@ -18,26 +18,12 @@ class Graph:
         for node in nodes:
             self.adj[node] = set()
 
-def data_to_n(data):
-    """Read initial one-, four- or eight-unit value from graph6
-    integer sequence.
-
-    Return (value, rest of seq.)"""
-    if data[0] <= 62:
-        return data[0], data[1:]
-    if data[1] <= 62:
-        return (data[1] << 12) + (data[2] << 6) + data[3], data[4:]
-    return (
-        (data[2] << 30)
-        + (data[3] << 24)
-        + (data[4] << 18)
-        + (data[5] << 12)
-        + (data[6] << 6)
-        + data[7],
-        data[8:],
-    )
 
 def decode_g6(bytes_in):
+    """
+    Decode a graph6 string into a graph object.
+    Source : networkx library - https://networkx.org/documentation/stable/_modules/networkx/readwrite/graph6.html#from_graph6_bytes (29/12/2022, 14:15)
+    """
     def bits():
         """Returns sequence of individual bits from 6-bit-per-value
             list of data values."""
@@ -45,6 +31,25 @@ def decode_g6(bytes_in):
             for i in [5, 4, 3, 2, 1, 0]:
                 yield (d >> i) & 1
 
+    def data_to_n(data):
+        """Read initial one-, four- or eight-unit value from graph6
+        integer sequence.
+        
+        Return (value, rest of seq.)"""
+        if data[0] <= 62:
+            return data[0], data[1:]
+        if data[1] <= 62:
+            return (data[1] << 12) + (data[2] << 6) + data[3], data[4:]
+        return (
+            (data[2] << 30)
+            + (data[3] << 24)
+            + (data[4] << 18)
+            + (data[5] << 12)
+            + (data[6] << 6)
+            + data[7],
+            data[8:],
+        )
+    
     if bytes_in.startswith(b">>graph6<<"):
         bytes_in = bytes_in[10:]
 
@@ -64,26 +69,23 @@ def decode_g6(bytes_in):
         if b:
             G.add_edge(i, j)
     return G
+# <<<=== END OF UTILITY ===>>>
 
-# CLIQUES algorithm
-def CLIQUES(SUBG: set, CAND: set, Q: list, G: Graph):
+def BK(SUBG: set, CAND: set, Q: list, G: Graph):
     if len(SUBG) == 0:
         print(" ".join(map(str, Q)))
     else:
-        u = max(SUBG, key=lambda u: len(CAND & G.adj[u]))
-        
-        for p in CAND - G.adj[u]:
+        for p in list(CAND):
             p_neighbors = G.adj[p]
             Q.append(p)
             SUBG_p = SUBG & p_neighbors
             CAND_p = CAND & p_neighbors
-            CLIQUES(SUBG_p, CAND_p, Q, G)
+            BK(SUBG_p, CAND_p, Q, G)
             Q.pop()
             CAND.remove(p)
-                
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     g6 = input()
     G = decode_g6(g6.encode())
-    CLIQUES(set(G.adj.keys()), set(G.adj.keys()), [], G)
-        
+    BK(set(G.adj.keys()), set(G.adj.keys()), [], G)
