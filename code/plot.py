b31cc645a85b88c47f5b31b2c67e9b8ec9698b30
VirgilSurin <virgil.surin@student.umons.ac.be>
2024-07-29 15:01:27 +0200

    exemple + images plot

--- code/plot.py
+++ code/plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,14 +13,19 @@ def read_data(file_path):
 def calculate_mean(data):
     return np.mean(data)
 
-def process_and_plot(typ, directory, g):
-    fns = ["CLIQUES", "BKP_R", "BKP_M"]
+
+def process_and_plot(typ, directory):
+    """
+    Plot les résultats des différents algo sur les graphes classiques.
+    """
+    fns = ["CLIQUES", "BKP_R", "BKP_M", "BK"]
     function_data = {fn: {} for fn in fns}
     orders = None
+    pattern = re.compile(rf'^{typ}_res_({{"|".join(fns)}})_(\d+)\.out$')
     for fn in fns:
         orders = set()
         for filename in os.listdir(directory):
-            if filename.startswith(f'{typ}_rust_res_{fn}_{g}') and filename.endswith('.out'):
+            if pattern.match(filename):
                 parts = filename.split('_')
                 order = int(parts[-1].split('.')[0])
 
@@ -46,12 +52,161 @@ def process_and_plot(typ, directory, g):
     plt.xlabel('Ordre')
     plt.ylabel("Temps d'exécution moyen (secondes)")
     plt.legend()
-    plt.savefig(f'./out_fig/{typ}_rust_pivot_{g}_plot.png')
+    plt.savefig(f'./out_fig/{typ}_plot.png')
 
-# Call the main function with the directory containing the files
-process_and_plot('total', './out/specials/', "complete")
-process_and_plot('total', './out/specials/', "empty")
-process_and_plot('total', './out/specials/', "turan")
-process_and_plot('delay', './out/specials/', "complete")
-process_and_plot('delay', './out/specials/', "empty")
-process_and_plot('delay', './out/specials/', "turan")
+
+def process_and_plot_special(typ, directory, g=None):
+    """
+    Plot les résultats des différents algo sur les graphes spéciaux.
+    """
+    fns = ["CLIQUES", "BKP_R", "BKP_M"]
+    function_data = {fn: {} for fn in fns}
+    orders = None
+    for fn in fns:
+        orders = set()
+        for filename in os.listdir(directory):
+            if filename.startswith(f'{typ}_res_{fn}_{g}') and filename.endswith('.out'):
+                parts = filename.split('_')
+                order = int(parts[-1].split('.')[0])
+
+                # Read data from file
+                data = read_data(os.path.join(directory, filename))
+
+                # Calculate mean
+                mean_value = calculate_mean(data)
+
+                # Store mean value for function and order
+                function_data[fn][order] = mean_value
+
+                # Add order to set of orders
+                orders.add(order)
+
+        # Convert orders set to sorted list
+        orders = sorted(list(orders))
+
+    plt.figure()
+    # Plotting
+    for fn, data in function_data.items():
+        plt.plot(orders, [data.get(order, 0) for order in orders], label=fn)
+
+    plt.xlabel('Ordre')
+    plt.ylabel("Temps d'exécution moyen (secondes)")
+    plt.legend()
+    plt.savefig(f'./out_fig/{typ}_pivot_{g}_plot.png')
+
+def process_and_plot_pyrust_special(typ, directory, g):
+    """
+    Compare algo par algo les résultats entre Rust et Python pour les graphes spéciaux
+    """
+    fns = ["CLIQUES", "BKP_R", "BKP_M"]
+    function_data = {fn: {} for fn in fns}
+    rust_data = {fn: {} for fn in fns}
+    orders = None
+    for fn in fns:
+        orders = set()
+        for filename in os.listdir(directory):
+            if filename.startswith(f'{typ}_res_{fn}_{g}') and filename.endswith('.out'):
+                parts = filename.split('_')
+                order = int(parts[-1].split('.')[0])
+
+                # Read data from file
+                data = read_data(os.path.join(directory, filename))
+
+                # Calculate mean
+                mean_value = calculate_mean(data)
+
+                # Store mean value for function and order
+                function_data[fn][order] = mean_value
+
+                # Add order to set of orders
+                orders.add(order)
+            elif filename.startswith(f'{typ}_rust_res_{fn}_{g}') and filename.endswith('.out'):
+                parts = filename.split('_')
+                order = int(parts[-1].split('.')[0])
+
+                # Read data from file
+                data = read_data(os.path.join(directory, filename))
+
+                # Calculate mean
+                mean_value = calculate_mean(data)
+
+                # Store mean value for function and order
+                rust_data[fn][order] = mean_value
+
+        # Convert orders set to sorted list
+        orders = sorted(list(orders))
+
+    # Plotting
+    for fn in fns:
+        plt.figure()
+        python_data = function_data[fn]
+        rust_data_fn = rust_data[fn]
+
+        plt.plot(orders, [python_data.get(order, 0) for order in orders], label=f'{fn} - Python')
+        plt.plot(orders, [rust_data_fn.get(order, 0) for order in orders], label=f'{fn} - Rust')
+
+        plt.xlabel('Ordre')
+        plt.ylabel("Temps d'exécution moyen (secondes)")
+        plt.legend()
+        plt.savefig(f'./out_fig/{typ}_{fn}_pyrust_pivot_{g}_plot.png')
+        plt.close()
+
+def process_and_plot_pyrust(typ, directory):
+    """
+    Compare algo par algo les résultats entre Rust et Python pour les graphes
+    """
+    fns = ["CLIQUES", "BKP_R", "BKP_M", "BK"]
+    function_data = {fn: {} for fn in fns}
+    rust_data = {fn: {} for fn in fns}
+    orders = None
+    pattern = re.compile(rf'^{typ}_res_({"|".join(fns)})_(\d+)\.out$')
+    rust_pattern = re.compile(rf'^{typ}_rust_res_({"|".join(fns)})_(\d+)\.out$')
+    for fn in fns:
+        orders = set()
+        for filename in os.listdir(directory):
+            if pattern.match(filename):
+                parts = filename.split('_')
+                order = int(parts[-1].split('.')[0])
+
+                # Read data from file
+                data = read_data(os.path.join(directory, filename))
+
+                # Calculate mean
+                mean_value = calculate_mean(data)
+
+                # Store mean value for function and order
+                function_data[fn][order] = mean_value
+
+                # Add order to set of orders
+                orders.add(order)
+            elif rust_pattern.match(filename):
+                parts = filename.split('_')
+                order = int(parts[-1].split('.')[0])
+
+                # Read data from file
+                data = read_data(os.path.join(directory, filename))
+
+                # Calculate mean
+                mean_value = calculate_mean(data)
+
+                # Store mean value for function and order
+                rust_data[fn][order] = mean_value
+
+        # Convert orders set to sorted list
+        orders = sorted(list(orders))
+
+    # Plotting
+    for fn, data in function_data.items():
+        plt.figure()
+        python_data = function_data[fn]
+        rust_data_fn = rust_data[fn]
+        plt.plot(orders, [python_data.get(order, 0) for order in orders], label=f'{fn} - Python')
+        plt.plot(orders, [rust_data_fn.get(order, 0) for order in orders], label=f'{fn} - Rust')
+        plt.xlabel('Ordre')
+        plt.ylabel("Temps d'exécution moyen (secondes)")
+        plt.legend()
+        plt.savefig(f'./out_fig/{typ}_pyrust_{fn}_plot.png')
+        plt.close()
+
+process_and_plot_pyrust('total', './out/')
+process_and_plot_pyrust('delay', './out/')
